15dc4428563c8e5e566b8789506468adb6f3c131
ManishGowda03 <manishsgowda10@gmail.com>
2026-02-24 23:18:16 +0530

    User Model

--- server/models/User.js
+++ server/models/User.js
@@ -5,15 +5,22 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
+      match: [
+        /^\S+@\S+\.\S+$/,
+        "Please enter a valid email address",
+      ],
     },
     password: {
       type: String,
       required: true,
+      minlength: 8, // minimum 8 characters
     },
     role: {
       type: String,
