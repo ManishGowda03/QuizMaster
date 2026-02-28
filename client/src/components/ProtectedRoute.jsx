060d3fa764feb105105dcb3e9e9d8bbbecefe64d
ManishGowda03 <manishsgowda10@gmail.com>
2026-02-28 23:17:44 +0530

    Updated Components

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,10 +1,17 @@
 import { Navigate } from "react-router-dom";
 
-function ProtectedRoute({ children }) {
+function ProtectedRoute({ children, adminOnly = false }) {
   const token = localStorage.getItem("token");
+  const storedUser = JSON.parse(localStorage.getItem("user"));
 
+  // If not logged in
   if (!token) {
     return <Navigate to="/login" replace />;
+  }
+
+  // If admin-only route but user is not admin
+  if (adminOnly && storedUser?.user?.role !== "admin") {
+    return <Navigate to="/" replace />;
   }
 
   return children;
