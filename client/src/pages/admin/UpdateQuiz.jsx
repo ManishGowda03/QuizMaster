ee4d2c749827e6f4c310566ae5cb398c427b541c
ManishGowda03 <manishsgowda10@gmail.com>
2026-03-01 20:57:59 +0530

    Update Quiz fpr Admin

---
+++ client/src/pages/admin/UpdateQuiz.jsx
@@ -0,0 +1,11 @@
+function UpdateQuiz() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white p-8">
+      <h1 className="text-3xl font-bold">
+        Update Quiz Page (Coming Soon)
+      </h1>
+    </div>
+  );
+}
+
+export default UpdateQuiz;
